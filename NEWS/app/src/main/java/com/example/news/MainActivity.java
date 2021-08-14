f5d474f58038b42f4c7b24c4508125f465b2e16d
SANDEEP GUPTA <guptasg300@gmail.com>
2021-08-14 22:13:15 +0530

    commit first

---
+++ NEWS/app/src/main/java/com/example/news/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.news;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        // Create an adapter that knows which fragment should be shown on each page
+        FixedTabPagerAdapter adapter = new FixedTabPagerAdapter(getSupportFragmentManager());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+
+}
